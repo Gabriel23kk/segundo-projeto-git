6b1cd8de16f5780f9bd55daa3dc16c4cea88137e
Gabriel23kk <ps996490891@gmail.com>
2023-04-20 09:57:25 -0300

    meu commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+    function trocarcor(cor){
+        let circulo = document.querySelector(".circulo")
+        circulo.style.background = cor 
+
+    }
+
+    function trocaimagem(imagem){
+        let imgiphone = document.querySelector(".iphone")
+        imgiphone.src = imagem
+    }
